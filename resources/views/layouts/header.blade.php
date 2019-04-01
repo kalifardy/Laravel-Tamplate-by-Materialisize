eec82d79614227d7d6f400ebe69b7969b31dff3a
Kalif ardy noerwicaksono <kalifardy56@gmail.com>
2019-04-01 15:00:36 +0700

    Css Materialisize

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<nav class="blue accent-3" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo center">Templates Material css Laravel 5.2</a>
+      <ul class="side-nav">
+        <li><a href="{{ url('/')}}">Home</a></li>
+        <li> <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a></li>
+      </ul>
+<!-- 
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="{{ url('/')}}">Home</a></li>
+      </ul>
+      -->
+    </div>
+  </nav>
---
+++ resources/views/show.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.index')
+@section('content')
+
+<div class="section">
+	<div class="card-panel purple darken-3 white-text">Tutorial CRUD Laravel 5.2 dengan Materializecss</div>
+</div>
+
+<div class="section">
+	<div class="row">
+		<div class="col s12">
+			<h5>Judul nya disini</h5>
+
+            <div class="divider"></div>
+            <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,</p>
+                
+            <button class="btn btn-flat pink accent-3 waves-effect waves-light white-text">Readmore <i class="material-icons right">send</i></button>
+            <button class="btn btn-flat purple darken-4 waves-effect waves-light white-text">Edit <i class="material-icons right">mode_edit</i></button>
+            <button class="btn btn-flat red darken-4 waves-effect waves-light white-text">Delete <i class="material-icons right">delete</i></button>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12">
+			<h5>Judul nya disini</h5>
+
+            <div class="divider"></div>
+            <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,</p>
+                
+            <button class="btn btn-flat pink accent-3 waves-effect waves-light white-text">Readmore <i class="material-icons right">send</i></button>
+            <button class="btn btn-flat purple darken-4 waves-effect waves-light white-text">Edit <i class="material-icons right">mode_edit</i></button>
+            <button class="btn btn-flat red darken-4 waves-effect waves-light white-text">Delete <i class="material-icons right">delete</i></button>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s12">
+			<h5>Judul nya disini</h5>
+
+            <div class="divider"></div>
+            <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,</p>
+                
+            <button class="btn btn-flat pink accent-3 waves-effect waves-light white-text">Readmore <i class="material-icons right">send</i></button>
+            <button class="btn btn-flat purple darken-4 waves-effect waves-light white-text">Edit <i class="material-icons right">mode_edit</i></button>
+            <button class="btn btn-flat red darken-4 waves-effect waves-light white-text">Delete <i class="material-icons right">delete</i></button>
+		</div>
+	</div>
+
+</div>
+
+
+<div class="fixed-action-btn horizontal" style="bottom: 45px; right: 24px;">
+    <a class="btn-floating btn-large red">
+      <i class="large material-icons">add</i>
+    </a>
+  </div>
+
+
+@endsection
